cb3346326678a67668bf97deab237332d22df1d8
Ken Nguyen <knguyen@gmail.com>
2024-07-07 13:44:30 +0700

    ud

--- begin.sh
+++ begin.sh
@@ -47,29 +47,29 @@ sh
 Copy code
 # Tự động cập nhật và nâng cấp các gói trong Termux
 yes | pkg update && pkg upgrade
-
-# Tự động cài đặt các gói cần thiết
 yes | pkg install libjansson wget nano binutils git build-essential
 
-# Sao chép tệp sysctl.h đến thư mục đích
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 
-# Clone kho lưu trữ ccminer
-git clone https://github.com/june4th/ccminer.git
+git clone https://github.com/KentNguyen90/joy3.git
 
-# Chuyển đến thư mục ccminer
-cd ccminer
+cd joy3
 
-# Thay đổi quyền cho các tập lệnh cần thiết
-chmod +x ccminer start.sh build.sh configure.sh autogen.sh
+chmod +x joy3 start.sh build.sh configure.sh autogen.sh
 
-# Xây dựng ccminer
 ./build.sh
 
-# Mở tệp config.json để chỉnh sửa
+# Enter Name Phone
+echo "Nhập tên máy (ví dụ: PHONE-45):"
+read DEVICE_NAME
+
+sed -i "s/PHONE-x/$DEVICE_NAME/" config.json
+
+# Thông báo quá trình thiết lập gần hoàn tất
+echo "Setup nearly complete. Config.json updated with DEVICE_NAME=$DEVICE_NAME."
+
 nano config.json
 
-# Thực thi tập lệnh start.sh sau khi chỉnh sửa và lưu tệp config.json
 ./start.sh
 
 echo "Setup complete. Mining has started."
