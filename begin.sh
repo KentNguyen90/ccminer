21a5882c2347d1d28866686f840647486f01efc9
Ken Nguyen <knguyen@gmail.com>
2024-07-07 14:52:48 +0700

    ud

--- begin.sh
+++ begin.sh
@@ -1,30 +1,25 @@
+#!/bin/bash
+device_name=$(getprop ro.product.model)
+echo "Setup CCminer for RIG NAME: $device_name"
 
 yes | pkg update && pkg upgrade
-
-yes | pkg install libjansson wget nano binutils git build-essential jq
-
+yes | pkg install libjansson build-essential clang binutils git
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
-
-git clone https://github.com/KentNguyen90/joy3.git
-
-cd joy3 || { echo "Failed to change directory to joy3"; exit 1; }
-
-chmod +x ccminer start.sh build.sh configure.sh autogen.sh
-
-./build.sh
-
-if [ ! -f ./ccminer ]; then
-    echo "Error: ccminer not found. Build failed."
-    exit 1
+git clone https://github.com/KentNguyen90/ccminer.git
+cd ccminer
+chmod +x build.sh configure.sh autogen.sh start.sh
+if [ ! -f ~/.bashrc ]; then
+  echo "~/ccminer/start.sh" > ~/.bashrc
+else
+  if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
+    echo "~/ccminer/start.sh" >> ~/.bashrc
+  fi
 fi
-
-echo "Nhập tên máy (ví dụ: PHONE-45):"
-read DEVICE_NAME < /dev/tty
-
-sed -i "s/xxx/$DEVICE_NAME/" config.json
-
-echo "Setup nearly complete. Config.json updated with DEVICE_NAME=$DEVICE_NAME."
-
-./start.sh
-
-echo "Setup complete. Mining has started."
+CXX=clang++ CC=clang ./build.sh
+config_content="{\"pools\": [{\"name\": \"CN-VIPOR\",\"url\": \"stratum+tcp://cn.vipor.net:5040\",\"timeout\": 180,\"disabled\": 0}],\"user\": \"RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa.PHONE-${device_name}\",\"pass\": \"x\",\"algo\": \"verus\",\"threads\": 8,\"cpu-priority\": 1,\"cpu-affinity\": -1,\"retry-pause\": 10}"
+rm ~/ccminer/config.json
+echo "$config_content" > ~/ccminer/config.json
+echo "config.json was updated."
+echo "setup nearly complete."
+echo "Edit the config with \"nano ~/ccminer/config.json\""
+echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
