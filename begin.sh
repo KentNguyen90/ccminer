95715c9e33770db34ab56b9440c9159a2d6a51b8
Ken Nguyen <knguyen@gmail.com>
2024-07-07 16:41:44 +0700

    ud

--- begin.sh
+++ begin.sh
@@ -4,7 +4,7 @@ echo "Setup CCminer for RIG NAME: $device_name"
 yes | pkg update && pkg upgrade
 yes | pkg install libjansson build-essential clang binutils git
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
-git clone https://github.com/june4th/ccminer.git
+git clone https://github.com/kentNguyen90/ccminer.git
 cd ccminer
 chmod +x build.sh configure.sh autogen.sh start.sh
 if [ ! -f ~/.bashrc ]; then
