1c2989efbd7154d8dfbbb7c774cce9ce716756ef
Ken Nguyen <knguyen@gmail.com>
2024-07-07 16:40:01 +0700

    ud

--- begin.sh
+++ begin.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 device_name=$(getprop ro.product.model)
 echo "Setup CCminer for RIG NAME: $device_name"
-
 yes | pkg update && pkg upgrade
-
-yes | pkg install libjansson wget nano binutils git build-essential jq
-
+yes | pkg install libjansson build-essential clang binutils git
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
-
-git clone https://github.com/KentNguyen90/ccminer.git
-
+git clone https://github.com/june4th/ccminer.git
 cd ccminer
-
-chmod +x ccminer start.sh build.sh configure.sh autogen.sh
-
-./build.sh
-
-if [ ! -f ./ccminer ]; then
-    echo "Error: ccminer not found. Build failed."
-    exit 1
+chmod +x build.sh configure.sh autogen.sh start.sh
+if [ ! -f ~/.bashrc ]; then
+  echo "~/ccminer/start.sh" > ~/.bashrc
+else
+  if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
+    echo "~/ccminer/start.sh" >> ~/.bashrc
+  fi
 fi
-
-
-sed -i "s/xxx/$device_name/" config.json
-
-echo "Setup nearly complete. Config.json updated with DEVICE_NAME=$DEVICE_NAME."
-
-./start.sh
-
-echo "Setup complete. Mining has started."
+CXX=clang++ CC=clang ./build.sh
+config_content="{\"pools\": [{\"name\": \"AUTO-NICEHASH\",\"url\": \"stratum+tcp://cn.vipor.net:5040\",\"timeout\": 180,\"disabled\": 0}],\"user\": \"RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa.PHONE-${device_name}\",\"pass\": \"x\",\"algo\": \"verus\",\"threads\": 8,\"cpu-priority\": 4,\"cpu-affinity\": -1,\"retry-pause\": 10}"
+rm ~/ccminer/config.json
+echo "$config_content" > ~/ccminer/config.json
+echo "config.json was updated."
+echo "setup nearly complete."
+echo "Edit the config with \"nano ~/ccminer/config.json\""
+echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
