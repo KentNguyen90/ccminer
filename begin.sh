4f9521618d5ebbf3500a72ee6891250192d266a2
Ken Nguyen <knguyen@gmail.com>
2024-07-07 14:24:48 +0700

    ud

--- begin.sh
+++ begin.sh
@@ -12,12 +12,14 @@ cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/
 # Clone kho lưu trữ joy3
 git clone https://github.com/KentNguyen90/joy3.git
 
+# Chuyển đến thư mục joy3
+cd joy3 || { echo "Failed to change directory to joy3"; exit 1; }
 
 # Thay đổi quyền cho các tập lệnh cần thiết
-chmod +x ./joy3/ccminer ./joy3/start.sh ./joy3/build.sh ./joy3/configure.sh ./joy3/autogen.sh
+chmod +x ccminer start.sh build.sh configure.sh autogen.sh
 
 # Xây dựng ccminer
-./joy3/build.sh
+./build.sh
 
 # Kiểm tra sự tồn tại của tệp ccminer
 if [ ! -f ./ccminer ]; then
@@ -30,15 +32,15 @@ echo "Nhập tên máy (ví dụ: PHONE-45):"
 read DEVICE_NAME < /dev/tty
 
 # Chỉnh sửa tệp config.json để thay thế PHONE-x bằng tên thiết bị
-sed -i "s/PHONE-x/$DEVICE_NAME/" ./joy3/config.json
+sed -i "s/-x/$DEVICE_NAME/" config.json
 
 # Thông báo quá trình thiết lập gần hoàn tất
 echo "Setup nearly complete. Config.json updated with DEVICE_NAME=$DEVICE_NAME."
 
 # Mở tệp config.json để chỉnh sửa (nếu cần)
-nano ./joy3/config.json
+nano config.json
 
 # Thực thi tập lệnh start.sh sau khi chỉnh sửa và lưu tệp config.json
-./joy3/start.sh
+./start.sh
 
 echo "Setup complete. Mining has started."
