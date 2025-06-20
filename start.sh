590495c707013cb88162868fa843b73914a0c66d
Ken Nguyen <knguyen@gmail.com>
2025-06-20 10:17:51 +0700

    update

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-
-device_name="PHONE-xxx"
+device_name="B6-6"
 wallet="RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa"
 
 CONFIG_URL="https://raw.githubusercontent.com/KentNguyen90/ccminer/refs/heads/main/config.json"
 CONFIG_FILE=~/ccminer/config.json
 
-# Tải file cấu hình từ URL
 curl -L -o "$CONFIG_FILE" "$CONFIG_URL"
 if [ $? -eq 0 ]; then
     echo "Đã cập nhật config.json thành công."
@@ -14,17 +12,13 @@ else
     echo "Tải config.json thất bại. Dùng cấu hình cũ (nếu có)."
 fi
 
-# Cập nhật nội dung file config.json
+
 if command -v jq >/dev/null 2>&1; then
-    jq --arg user "$wallet.$device_name" \
-       '
-       .user = $user
-       ' "$CONFIG_FILE" > "$CONFIG_FILE.tmp" && mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
+    jq --arg user "$wallet.$device_name" '.user = $user' "$CONFIG_FILE" > "$CONFIG_FILE.tmp" && mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
     echo "Đã cập nhật giá trị user trong config.json"
 else
     echo "❗ Cảnh báo: jq chưa được cài, không thể cập nhật config.json"
     echo "→ Cài jq bằng: pkg install jq"
 fi
 
-# Khởi động ccminer
 ~/ccminer/ccminer -c "$CONFIG_FILE"
